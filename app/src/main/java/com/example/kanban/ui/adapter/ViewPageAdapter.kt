49aa0cf4f2be07e8c846911d142b7e603283d6e9
lorranCM <lorrancalazans@gmail.com>
2026-03-16 17:59:23 -0300

    pag 51

---
+++ app/src/main/java/com/example/kanban/ui/adapter/ViewPageAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.kanban.ui
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import java.util.ArrayList
+
+class ViewPageAdapter {
+    ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+
+
+
+    }
